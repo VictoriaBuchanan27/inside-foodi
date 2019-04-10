16900c5cf9bac2ef76836108c768d78682309618
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-04-09 21:41:34 -0400

    component user display created and cards added for frontend display still needs work

---
+++ src/components/userdisplay.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const UserDisplay =()=>{
+    return(
+        <>
+        <div class="card border-success mb-3" style={{maxWidth: '18rem'}}>
+            <div class="card-header">User Name</div>
+            <div class="card-body text-success">
+                <h5 class="card-title">Success card title</h5>
+                <p class="card-text">Users Restaurant Review Here</p>
+            </div>
+        </div>
+        <div class="card border-success mb-3" style={{maxWidth: '18rem'}}>
+            <div class="card-header">User Name</div>
+            <div class="card-body text-success">
+                <h5 class="card-title">Success card title</h5>
+                <p class="card-text">Users Restaurant Review Here</p>
+            </div>
+        </div>
+        <div class="card border-success mb-3" style={{maxWidth: '18rem'}}>
+            <div class="card-header">User Name</div>
+            <div class="card-body text-success">
+                <h5 class="card-title">Success card title</h5>
+                <p class="card-text">Users Restaurant Review Here</p>
+            </div>
+        </div>
+        </>
+    )
+}
+export default UserDisplay;
