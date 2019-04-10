7960a47e80a1ab9096a943093c1890cc99b99e7e
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-04-09 21:40:30 -0400

    bio component for the profile created not done

---
+++ src/components/userbio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserPicture from './userpicture';
+// import UserPic from './userpicture';
+
+const UserBio =(props)=>{
+    return(
+        <>
+        <UserPicture />
+        
+        </>
+    )
+}
+export default UserBio;
