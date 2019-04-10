a995780afe7d58c504c3a4d3853ddb919d410f16
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-04-10 14:10:45 -0400

    user picture working, still need to add the option of the user changing picture in settings

--- src/components/userpicture.js
+++ src/components/userpicture.js
@@ -1,9 +1,12 @@
 import React from 'react';
+// import './userpicture.css';
 
 const UserPicture =(props)=>{
     return(
         <>
-
+        <div className="userImage">
+          <img alt = 'userImage' src={require('./foodi.png')}  />
+        </div>
         </>
     )
 }
