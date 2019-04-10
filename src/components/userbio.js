7c457068d0bb74366770efc144880d7610b4797e
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-04-10 14:09:40 -0400

    user bio section being created, user picture added still needs work

--- src/components/userbio.js
+++ src/components/userbio.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import UserPicture from './userpicture';
-// import UserPic from './userpicture';
+
 
 const UserBio =(props)=>{
     return(
         <>
-        <UserPicture />
-        
+        <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">UserName</h1>
+            <UserPicture />
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        </div>
+        </div>
         </>
     )
 }
