b2851e70fdad886c9127a46e1891346a9b789bb0
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-04-10 14:11:58 -0400

    user display layout is showing in browser, still need to put into rows and not hard code images

--- src/components/userdisplay.js
+++ src/components/userdisplay.js
@@ -6,22 +6,24 @@ const UserDisplay =()=>{
         <div class="card border-success mb-3" style={{maxWidth: '18rem'}}>
             <div class="card-header">User Name</div>
             <div class="card-body text-success">
-                <h5 class="card-title">Success card title</h5>
-                <p class="card-text">Users Restaurant Review Here</p>
+                <h5 class="card-title">Users Restaurant Review Here</h5>
+                <p class="card-text">Restaurant Location</p>
+            </div>
+        </div>
+        <img src={require('./foodi.png')}  class="rounded float-left" alt="mockpic"/>
+        <img src={require('./foodi.png')} class="rounded float-right" alt="..."/>
+        <div class="card border-success mb-3" style={{maxWidth: '18rem'}}>
+            <div class="card-header">User Name</div>
+            <div class="card-body text-success">
+                <h5 class="card-title">Users Restaurant Review Here</h5>
+                <p class="card-text">Restaurant Location</p>
             </div>
         </div>
         <div class="card border-success mb-3" style={{maxWidth: '18rem'}}>
             <div class="card-header">User Name</div>
             <div class="card-body text-success">
-                <h5 class="card-title">Success card title</h5>
-                <p class="card-text">Users Restaurant Review Here</p>
-            </div>
-        </div>
-        <div class="card border-success mb-3" style={{maxWidth: '18rem'}}>
-            <div class="card-header">User Name</div>
-            <div class="card-body text-success">
-                <h5 class="card-title">Success card title</h5>
-                <p class="card-text">Users Restaurant Review Here</p>
+                <h5 class="card-title">Users Restaurant Review Here</h5>
+                <p class="card-text">Restaurant Location and Time</p>
             </div>
         </div>
         </>
